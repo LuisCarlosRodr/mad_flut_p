5b11ac3d4f47bfdecceed9de899df3fadf5f2e92
jdieztejeda <jdieztejeda@gmail.com>
2024-06-06 19:46:56 +0200

    week 11(falta revisar)

--- lib/screens/second_screen.dart
+++ lib/screens/second_screen.dart
@@ -1,14 +1,69 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-class SecondScreen extends StatelessWidget {
+import 'package:intl/intl.dart';
+import 'package:path_provider/path_provider.dart';
+class SecondScreen extends StatefulWidget {
+  @override
+  _SecondScreenState createState() => _SecondScreenState();
+}
+class _SecondScreenState extends State<SecondScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _loadCoordinates();
+    print("initState: Initial state setup.");
+  }
+  Future<void> _loadCoordinates() async {
+    final directory = await getApplicationDocumentsDirectory();
+    final file = File('${directory.path}/gps_coordinates.csv');
+    List<String> lines = await file.readAsLines();
+    setState(() {
+      _coordinates = lines.map((line) => line.split(';')).toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    // Similar to the build method of a StatelessWidget,
+    // this method is called every time the widget needs to be rebuilt, for example, after calling setState().
+    print("build: Building the user interface.");
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text('Second screen'),
       ),
-      body: Center(
-        child: Text('Welcome to the Second Screen!'),
+      body: ListView.builder(
+        itemCount: _coordinates.length,
+        itemBuilder: (context, index) {
+          var coord = _coordinates[index];
+          var formattedDate = DateFormat('yyyy/MM/dd HH:mm:ss').format(DateTime.fromMillisecondsSinceEpoch(int.parse(coord[0])))
+          return ListTile(
+            title: Text('Timestamp: $formattedDate'),
+            subtitle: Text('Latitude: ${coord[1]}, Longitude: ${coord[2]}'),
+          );
+        },
       ),
     );
   }
 }
+
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    // This method is called right after initState the first time
+    // the widget is built and when any dependencies of the InheritedWidget change.
+    print("didChangeDependencies: Dependencies updated.");
+  }
+  @override
+  void didUpdateWidget(SecondScreen oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    // If the parent widget changes and has to rebuild this widget (because it needs to update the configuration),
+    // this method is called with the old widget as an argument.
+    print("didUpdateWidget: The widget has been updated from the parent.");
+  }
+  @override
+  void dispose() {
+    // This method is called when this state object is permanently removed.
+    print("dispose: Cleaning up before the state is destroyed.");
+    super.dispose();
+  }
+}
+
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,14 +1,154 @@
+import 'dart:io';
+import 'dart:async';
 import 'package:flutter/material.dart';
-class SplashScreen extends StatelessWidget {
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:logger/logger.dart';
+import 'settings_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+class _SplashScreenState extends State<SplashScreen> {
+  final logger = Logger();
+  final _uidController = TextEditingController();
+  final _tokenController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    _loadPrefs();
+  }
+  Future<void> _loadPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    String? uid = prefs.getString('uid');
+    String? token = prefs.getString('token');
+    if (uid == null || token == null) {
+      _showInputDialog();
+    } else {
+      logger.d("UID: $uid, Token: $token");
+    }
+  }
+
+  Future<void> _showInputDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Enter UID and Token'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                TextField(
+                  controller: _uidController,
+                  decoration: InputDecoration(hintText: "UID"),
+                ),
+                TextField(
+                  controller: _tokenController,
+                  decoration: InputDecoration(hintText: "Token"),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Save'),
+              onPressed: () async {
+                final prefs = await SharedPreferences.getInstance();
+                await prefs.setString('uid', _uidController.text);
+                await prefs.setString('token', _tokenController.text);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text("Flutter App"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SettingsScreen()),
+              );
+            },
+          ),
+        ],
       ),
       body: Center(
-        child: Text('Welcome to the Home Screen!'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Welcome to the Home Screen!'),
+            Switch(
+              value: _positionStreamSubscription != null,
+              onChanged: (value) {
+                setState(() {
+                  if (value) {
+                    startTracking();
+                  } else {
+                    stopTracking();
+                  }
+                });
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
+
+  void startTracking() async {
+    final locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.high, // Adjust the accuracy as needed
+      distanceFilter: 10, // Distance in meters before an update is triggered
+    );
+    final serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+    var permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error('Location permissions are permanently denied');
+    }
+    _positionStreamSubscription = Geolocator.getPositionStream(locationSettings: locationSettings).listen(
+          (Position position) {
+        writePositionToFile(position);
+      },
+    );
+  }
+  void stopTracking() {
+    _positionStreamSubscription?.cancel();
+    _positionStreamSubscription = null;
+  }
+  Future<void> writePositionToFile(Position position) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final file = File('${directory.path}/gps_coordinates.csv');
+    String timestamp = DateTime.now().millisecondsSinceEpoch.toString();
+    await file.writeAsString('${timestamp};${position.latitude};${position.longitude}\n', mode: FileMode.append);
+  }
+
+  @override
+  void dispose() {
+    _uidController.dispose();
+    _tokenController.dispose();
+    super.dispose();
+  }
 }
+
